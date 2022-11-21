018987a124b4e1656d7a5c0722a28655f23e5304
최은녕 <djflsth@naver.com>
2022-11-21 11:34:41 +0900

    [MODIFY] AptController 수정중!!(추후 수정마저 해야함)
    Former-commit-id: d57bfa045a75347597111bb0d77543d13635a930

--- Backend/src/main/java/com/ssafy/happyhouse/apt/controller/AptController.java
+++ Backend/src/main/java/com/ssafy/happyhouse/apt/controller/AptController.java
@@ -1,5 +1,6 @@
 package com.ssafy.happyhouse.apt.controller;
 
+import java.sql.SQLException;
 import java.util.List;
 
 import org.slf4j.Logger;
@@ -9,10 +10,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,8 +25,6 @@ import io.swagger.annotations.ApiOperation;
 @RequestMapping("/apt")
 @CrossOrigin("*")
 public class AptController {
-	private static final String SUCCESS = "success";
-	private static final String FAIL = "fail";
 	
 	private final Logger logger = LoggerFactory.getLogger(AptController.class);
 
@@ -74,16 +69,28 @@ public class AptController {
 	
 
 	@GetMapping("/address")
-	public ResponseEntity<SidoGugunCodeDto> getAddressName(@RequestParam("dongcode")String dongcode) throws Exception{
-		logger.debug("dongCode : {}", dongcode);
-		return new ResponseEntity<SidoGugunCodeDto>(aptService.getAddressName(dongcode), HttpStatus.OK);
+	public ResponseEntity<?> getAddressName(@RequestParam("dongcode")String dongcode) throws Exception{
+		try {
+			SidoGugunCodeDto dto = aptService.getAddressName(dongcode);
+			if(dto != null) {
+				return new ResponseEntity<SidoGugunCodeDto>(dto, HttpStatus.OK);
+			}else {
+				return new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
+			}
+		} catch (SQLException e) {
+			return exceptionHandling(e);
+		}
 	}
 	
-	 @ApiOperation(value = "아파트 조회", notes = "아파트 코드로 아파트 정보와 리뷰 정보를 조회한다.", response = String.class)
-		@GetMapping("/aptinfo")
-		public ResponseEntity<HouseInfoDto> selectHouse(@RequestParam String aptCode) throws Exception {
-			logger.debug("aptCode : {}", aptCode);
-			logger.debug("result : {}", aptService.selectHouse(aptCode));
-			return new ResponseEntity<HouseInfoDto>(aptService.selectHouse(aptCode), HttpStatus.OK);
+	@ApiOperation(value = "아파트 조회", notes = "아파트 코드로 아파트 정보와 리뷰 정보를 조회한다.", response = String.class)
+	@GetMapping("/aptinfo")
+	public ResponseEntity<HouseInfoDto> selectHouse(@RequestParam String aptCode) throws Exception {
+		logger.debug("aptCode : {}", aptCode);
+		logger.debug("result : {}", aptService.selectHouse(aptCode));
+		return new ResponseEntity<HouseInfoDto>(aptService.selectHouse(aptCode), HttpStatus.OK);
 	}
+	 
+	 private ResponseEntity<String> exceptionHandling(SQLException e) {
+		 return new ResponseEntity<String>("Error : " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+	 }
 }
