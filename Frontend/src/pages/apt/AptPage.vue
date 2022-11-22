019f1dc3e99c9f29ba2a57ef090b95bf066cf3de
최은녕 <djflsth@naver.com>
2022-11-22 11:50:47 +0900

    [MODIFY] AptPage 검색 부분 위치 가운데 고정
    Former-commit-id: 29cd387efa4c843d2336f6d30277e9531845ef04

--- Frontend/src/pages/apt/AptPage.vue
+++ Frontend/src/pages/apt/AptPage.vue
@@ -1,26 +1,30 @@
 <template>
   <div class="page-header clear-filter" filter-color="orange">
-    <div class="content" style="margin-top: 130px">
-      <div class="row col-md-12 justify-content-center mb-4">
-        <apt-search-form></apt-search-form>
-        <b-row class="mb-5">
-          <b-col cols="1" class="p-0"> </b-col>
-          <b-col cols="3" class="p-0">
-            <apt-list> </apt-list>
-          </b-col>
-          <b-col class="p-0">
-            <apt-map v-on:show-modal="showModal"></apt-map>
-          </b-col>
-        </b-row>
+    <b-container class="bv-example-row md-3">
+      <div class="content">
+        <div class="row col-md-12 justify-content-center mb-4">
+          <div class="col-md-10 ml-auto mr-auto">
+            <apt-search-form></apt-search-form>
+          </div>
+          <b-row class="mb-5">
+            <b-col cols="1" class="p-0"> </b-col>
+            <b-col cols="3" class="p-0">
+              <apt-list> </apt-list>
+            </b-col>
+            <b-col class="p-0">
+              <apt-map v-on:show-modal="showModal"></apt-map>
+            </b-col>
+          </b-row>
+        </div>
       </div>
-    </div>
-    <b-modal
-      id="modal-lg"
-      size="lg"
-      v-model="modalShow"
-      title="아파트 상세 조회">
-      <apt-detail></apt-detail>
-    </b-modal>
+      <b-modal
+        id="modal-lg"
+        size="lg"
+        v-model="modalShow"
+        title="아파트 상세 조회">
+        <apt-detail></apt-detail>
+      </b-modal>
+    </b-container>
   </div>
 </template>
 <script>
