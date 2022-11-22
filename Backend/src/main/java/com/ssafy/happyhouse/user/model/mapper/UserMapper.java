e125deaa9fd56cc089b7f0f3def688987fc8497f
나유현 <12nyk34@naver.com>
2022-11-22 09:31:59 +0900

    [ADD] 관심지역 기능 추가
    Former-commit-id: a603fe651ea8df3af51e217ffc23187f6a191a92

--- Backend/src/main/java/com/ssafy/happyhouse/user/model/mapper/UserMapper.java
+++ Backend/src/main/java/com/ssafy/happyhouse/user/model/mapper/UserMapper.java
@@ -1,10 +1,12 @@
 package com.ssafy.happyhouse.user.model.mapper;
 
 import java.sql.SQLException;
+import java.util.List;
 import java.util.Map;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import com.ssafy.happyhouse.apt.model.SidoGugunCodeDto;
 import com.ssafy.happyhouse.user.model.UserDto;
 
 @Mapper
@@ -20,4 +22,10 @@ public interface UserMapper {
 	public void saveRefreshToken(Map<String, String> map) throws SQLException;
 	public Object getRefreshToken(String userid) throws SQLException;
 	public void deleteRefreshToken(Map<String, String> map) throws SQLException;
+	//관심지역 등록
+	void addLikeDong(Map<String, String>map)throws Exception;
+	//관심지역 제거
+	int deleteLikeDong(Map<String, String> map)throws Exception;
+	//관심지역 조회
+	List<SidoGugunCodeDto> listLikeDong(String userid)throws Exception;
 }
--- Backend/src/main/java/com/ssafy/happyhouse/user/model/service/UserService.java
+++ Backend/src/main/java/com/ssafy/happyhouse/user/model/service/UserService.java
@@ -1,7 +1,9 @@
 package com.ssafy.happyhouse.user.model.service;
 
+import java.util.List;
 import java.util.Map;
 
+import com.ssafy.happyhouse.apt.model.SidoGugunCodeDto;
 import com.ssafy.happyhouse.user.model.UserDto;
 
 public interface UserService {
@@ -20,4 +22,12 @@ public interface UserService {
 	UserDto getUser(String userId) throws Exception;
 	String findPwd(String userId) throws Exception;
 	void makePwd(Map<String, String> map) throws Exception;
+	
+	// 관심지역 등록
+	void addLikeDong(Map<String, String> map) throws Exception;
+
+	// 관심지역 조회
+	List<SidoGugunCodeDto> listLikeDong(String userid) throws Exception;
+	boolean deleteLikeDong(Map<String, String> map) throws Exception;
+
 }
